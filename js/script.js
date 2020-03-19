aebfd811f44eabe003e570df3156e4d8632e0d1b
nabeelmahdee <nabeelmahdee@users.noreply.github.com>
2020-03-19 13:36:45 -0400

    made the bars rotate and a few css changes

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(
+    function() {
+        const bars = $(".bars");
+        const ddlinks = $(".ddlinks");
+        const dditems = $(".dditems");
+        const topbarlinks = $(".topbar a");
+
+        ddlinks.css("color","black");
+        topbarlinks.css("color","black");
+        topbarlinks.css("text-decoration","none");
+
+        bars.click(function () {
+            dditems.toggle(200);
+            bars.toggleClass("rotate");
+        });
+    }  
+  ); // end of doc ready
