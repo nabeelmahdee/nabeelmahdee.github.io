60143b7a66ee707dbf3301d30cec2ce877bda478
nabeelmahdee <nabeelmahdee@users.noreply.github.com>
2020-11-26 02:15:17 -0500

    newer version done using materialize css

--- script/script.js
+++
@@ -1,5 +0,0 @@
-gsap.from('.horizontal-line', { duration: .7, y:'-100%', opacity: 0, ease: 'power1',delay: .2 })
-gsap.from('.vertical-line', { duration: .7, x:'-100%', opacity: 0, ease: 'power1' })
-gsap.from('.tag-line', { duration: .9, y:'-100%', opacity: 0, ease: 'power4', delay: .3 })
-
-
